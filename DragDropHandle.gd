7234850899403509887b39eb7c07cd0af6b9a0c4
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-03-11 10:57:47 +0100

    Move around a shape

---
+++ DragDropHandle.gd
@@ -0,0 +1,26 @@
+extends Polygon2D
+
+var drag_offset : Vector2 = Vector2(0, 0)
+var is_dragging : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.s
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if is_dragging:
+		var mouse_at = get_global_mouse_position()
+		self.position = mouse_at + drag_offset;
+
+# Called when the mouse is pressed
+func _unhandled_input(event):
+	print(event)
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.pressed:
+				var mouse_at = get_global_mouse_position()
+				drag_offset = self.position - mouse_at
+				is_dragging = true
+			else:
+				is_dragging = false
