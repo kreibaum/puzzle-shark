0c2fb4d0fb68bf43d7856d325cc74ffb707318c9
Thomas Staudt <tscode@posteo.net>
2023-03-31 22:00:59 +0200

    fixed the nasty c++ exception when edge length was miniscule

--- Edge/EdgeCollisionArea.gd
+++ Edge/EdgeCollisionArea.gd
@@ -1,5 +1,13 @@
 class_name EdgeCollisionArea extends Area2D
 
+var disabled = false
+
+func is_disabled():
+	return self.disabled
+
+func disable():
+	$Polygon.set_polygon(PackedVector2Array())
+	self.disabled = true
 
 # Create a polygon box
 func box(center, radius):
@@ -10,28 +18,32 @@ func box(center, radius):
 	points.append(center + radius * Vector2(-1, 1))
 	return points
 
-
 # Recalculate the polygon for the collision area
 # TODO: What happens if the diameter of the edge shape is not much greater
 # or even smaller than thickness?
 func recalculate(edge_shape: PackedVector2Array, thickness: float = 5.0):
-	#print("recalculating collision area")
-	#var hull = hull_polygon(edge_shape, thickness)
-	var polys = Geometry2D.offset_polyline(edge_shape, thickness)
-	if len(polys) == 0:
-		return
-	var hull = polys[0]
+	var baseline = edge_shape[0] - edge_shape[-1]
+	var hull = PackedVector2Array()
 
-	var left_clip = box(edge_shape[0], 3 * thickness)
-	polys = Geometry2D.clip_polygons(hull, left_clip)
-	if len(polys) > 0:
+	if baseline.length() > 0:
+		var polys = Geometry2D.offset_polyline(edge_shape, thickness)
+		if len(polys) == 0: return
 		hull = polys[0]
 
-	var right_clip = box(edge_shape[-1], 3 * thickness)
-	polys = Geometry2D.clip_polygons(hull, right_clip)
-	if len(polys) > 0:
-		hull = polys[0]
+		var cutfraction = baseline.length() / (2 * thickness)
+		var cutradius = min(cutfraction, 3) * thickness
 
+		var left_clip = box(edge_shape[0], cutradius)
+		polys = Geometry2D.clip_polygons(hull, left_clip)
+		if len(polys) > 0:
+			hull = polys[0]
+
+		var right_clip = box(edge_shape[-1], cutradius)
+		polys = Geometry2D.clip_polygons(hull, right_clip)
+		if len(polys) > 0:
+			hull = polys[0]
+
+	self.disabled = false
 	$Polygon.set_polygon(hull)
 
 
